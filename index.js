f4d19a0416fa90834770c3e5e0050846c1820fd9
aroary <71286008+aroary@users.noreply.github.com>
2021-06-08 22:58:40 -0400

    Create index.js
    base of code

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login("ODUyMDE4NjM4MzY5MDYyOTEz.YMAuMQ.2t12vOxOWcZ5MazL6X2mVKifQ1E");
