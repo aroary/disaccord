b598e750c9c3a92ff437b6f941049a80060b6623
aroary <menachem@shkedi.com>
2021-06-21 08:43:51 -0400

    log bug fixed

--- core/utilities/logger.js
+++ core/utilities/logger.js
@@ -1,14 +1,13 @@
 const chalk = require('chalk');
 const moment = require("moment");
-const ts = `[${moment().format('YYYY-MM-DD HH:mm:ss')}]:`;
-
 module.exports = {
-    log:(data) => console.log(`${ts} ${chalk.black.bgWhite("LOG       ")} ${data}`),
-    command:(data) => console.log(`${ts} ${chalk.bgMagenta("COMMAND   ")} ${data}`),
-    warn:(data) => console.log(`${ts} ${chalk.black.bgYellow("WARN      ")} ${data}`),
-    error:(data) => console.log(`${ts} ${chalk.bgRed("ERROR     ")} ${data}`),
-    rateLimit:(data) => console.log(`${ts} ${chalk.bgCyan("RATELIMIT ")} ${data}`),
-    load:(data) => console.log(`${ts} ${chalk.bgGray("LOAD      ")} ${data}`),
-    ready:(client) => console.log(`${ts} ${chalk.bgGreen("READY     ")} ${client.user.username} is online in ${client.guilds.cache.size} servers for ${client.users.cache.size} users!`),
-    presence:(type, name) => console.log(`${ts} ${chalk.bgBlue("STATUS    ")} ${type} ${name}`)
+    // Log types
+    log:(data) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.black.bgWhite("LOG       ")} ${data}`),
+    command:(data) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.bgMagenta("COMMAND   ")} ${data}`),
+    warn:(data) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.black.bgYellow("WARN      ")} ${data}`),
+    error:(data) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.bgRed("ERROR     ")} ${data}`),
+    rateLimit:(data) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.bgCyan("RATELIMIT ")} ${data}`),
+    load:(data) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.bgGray("LOAD      ")} ${data}`),
+    ready:(client) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.bgGreen("READY     ")} ${client.user.username} is online in ${client.guilds.cache.size} servers for ${client.users.cache.size} users!`),
+    presence:(type, name) => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${chalk.bgBlue("STATUS    ")} ${type} ${name}`)
 };
