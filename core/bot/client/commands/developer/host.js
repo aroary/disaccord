ea8af674375b7c8299b2ac4e50f07e31c74cc881
aroary <meanchemshkedi@gmail.com>
2021-07-19 23:51:59 -0400

    host data

--- core/bot/client/commands/developer/host.js
+++ core/bot/client/commands/developer/host.js
@@ -5,7 +5,7 @@ module.exports = {
         info: {
             name: "host",
             usage: "host",
-            description: "Get host machine data and specs",
+            description: "Get host machine data and specs 🖥️",
             aliases: ["os", "ip"],
             permissions: ["SEND_MESSAGES"]
         },
