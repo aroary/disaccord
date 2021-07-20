069f5c484754be14a3355da2063aa88058c13a72
aroary <meanchemshkedi@gmail.com>
2021-07-19 23:40:00 -0400

    login reattempt

--- index.js
+++ index.js
@@ -10,19 +10,27 @@ const client = new Discord.Client();
 // Load secrets.
 var secrets = fs.readFileSync("./.secrets.json", { encoding: "utf-8" });
 secrets = JSON.parse(secrets);
-client.secrets = secrets;
 client.config = secrets;
-client.settings = secrets;
 
 // Initiate bot.
 require("./core/initiate/initiate")(client);
 
 // Login.
-client.login(client.secrets.token)
+client.login(client.config.token)
 	.then(() => {
 		client.online = true;
-		client.log.log("Logged in, getting ready...");
 	})
-	.catch(() => {
+	.catch(async () => {
 		client.log.error("Failed to log in. Check token and internet connection.");
+		client.log.warn("Reattempting to log in, in 300000 ms.");
+		await require("./core/utilities/delay").delay(300000);
+		client.login(client.config.token)
+			.then(() => {
+				client.online = true;
+			})
+			.catch(() => {
+				client.log.error("Failed to log in. Check token and internet connection.");
+				client.log.warn("Process is exiting");
+				process.exit(0);
+			});
 	});
