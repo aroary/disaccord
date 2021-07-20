44d31e75d151cf3b99b720bc127002c3c90866b8
aroary <meanchemshkedi@gmail.com>
2021-07-19 23:41:16 -0400

    delay action

--- core/utilities/delay.js
+++ core/utilities/delay.js
@@ -1,11 +1,19 @@
-// Delay version.
+/**
+ * @description Delay version.
+ * @param {number} ms - Number of milliseconds to wait. 
+ * @returns {Promise}
+ */
 function delay(ms) {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
     });
 };
-
-// Sleep version.
+/**
+ * @description Sleep version.
+ * @param {number} ms - Number of milliseconds to wait. 
+ * @returns {Promise}
+ * @deprecated
+ */
 function sleep(ms) {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
