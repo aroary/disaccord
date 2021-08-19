fb833a39bb5a5797170f2034fefe1d47a8d04b73
aroary <meanchemshkedi@gmail.com>
2021-08-19 10:37:33 -0400

    permissions fix

--- core/utilities/checkPermissions.js
+++ core/utilities/checkPermissions.js
@@ -10,27 +10,29 @@ const perms = require("../../documentation/permissions.json");
 
 /**
  * @description Checks for permissions for client and user.
+ * @param {discord.Channel} channel - The channel to check in.
  * @param {discord.GuildMember} client - Client to check.
  * @param {discord.GuildMember} member - Member to check.
  * @param {Array} permission - Array of permissions.
  * @returns {Object} Returns false if client and member have all requred permissions.
+ * @example checkPermissions(channel, guild.me, member, [])
  */
-module.exports = (client = null, member = null, permission) => {
+module.exports = (channel, client = null, member = null, permission) => {
     const missing = {};
     if (client) missing.client = [];
-    if (user) missing.user = [];
+    if (member) missing.user = [];
 
     permission.forEach(perm => {
         if (!perms.includes(perm)) throw new Error('Invalid Permission in permission array.');
 
         // Check for permissions for both client and member.
-        if (missing.client && !client.hasPermission(perm, { checkAdmin: true, checkOwner: true })) missing.client.push(perm);
-        if (missing.user && !member.hasPermission(perm, { checkAdmin: true, checkOwner: true })) missing.user.push(perm);
+        if (missing.client && !client.permissionsIn(channel.id).has(perm, true)) missing.client.push(perm);
+        if (missing.user && !member.permissionsIn(channel.id).has(perm, true)) missing.user.push(perm);
     });
 
     // Return results.
-    if (missing.client && !missing.user.length) missing.user = false;
-    if (missing.user && !missing.client.length) missing.client = false;
+    if (missing.user && !missing.user.length) missing.user = false;
+    if (missing.client && !missing.client.length) missing.client = false;
     if (missing.user || missing.client) return missing;
     else return false;
 };
