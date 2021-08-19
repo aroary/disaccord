36499fcb7302e30aa14b87f1937023a0434554b5
aroary <meanchemshkedi@gmail.com>
2021-08-18 23:22:36 -0400

    ready data

--- core/bot/server/events/ready.js
+++ core/bot/server/events/ready.js
@@ -7,16 +7,19 @@ const Entry = require("../../../utilities/logger");
  * @param {discord.Client} client - The client
  */
 function run(client) {
+    new Entry().setName("ready").setValue(`${client.user.username} is online with the ${client.secrets.intents} intents.`).setColor("green").log();
+
+    // Load data for slash commands
     if (client.secrets.redeploy) fs.readdir("core/bot/client/slashCommands", (error, files) => {
         if (error) return new Entry().setName("error").setValue(error).setColor("red");
         files.filter(file => file.split(".").pop() === "js").forEach(file => {
             // Get and create slash command.
             const slashCommand = require(`../../client/slashCommands/${file}`);
+            
+            // Register the slash command data.
             client.application?.commands.create(slashCommand.data);
         });
     });
-
-    new Entry().setName("ready").setValue(`${client.user.username} is online!`).setColor("green").log();
 };
 
 module.exports = { name: "ready", run };
