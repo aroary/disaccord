349b22acd0dda3fd8466c9666ffdf60906096307
aroary <meanchemshkedi@gmail.com>
2021-08-22 09:16:05 -0400

    better deployment

--- core/bot/client/commands/developer/deploy.js
+++ core/bot/client/commands/developer/deploy.js
@@ -24,22 +24,40 @@ function run(client, message, args) {
             });
         });
     } else {
-        // Load data for slash commands.
-        fs.readdir("core/bot/client/slashCommands", (error, files) => {
-            if (error) return new Entry().setName("error").setValue(error).setColor("red");
+        if (client.deployed) {
+            const e = new discord.MessageEmbed()
+                .setTitle("Deployment")
+                .setDescription("Already deployed.")
+                .setTimestamp();
+            message.channel.send({ embeds: [e] });
+        } else {
+            // Load data for slash commands.
+            fs.readdir("core/bot/client/slashCommands", (error, files) => {
+                if (error) return new Entry().setName("error").setValue(error).setColor("red");
 
-            // Loop through the slash command files to deploy.
-            files.filter(file => file.split(".").pop() === "js").forEach(file => {
-                // Get and create slash command.
-                const slashCommand = require(`../../slashCommands/${file}`);
+                var deployed = new discord.MessageEmbed()
+                    .setTitle("Deployment")
+                    .setTimestamp();
 
-                // Register the slash command data.
-                if (slashCommand.configuration.deploy) client.application?.commands.create(slashCommand.data);
+                var files = "";
+
+                // Loop through the slash command files to deploy.
+                files.filter(file => file.split(".").pop() === "js").forEach(file => {
+                    // Get and create slash command.
+                    const slashCommand = require(`../../slashCommands/${file}`);
+
+                    // Register the slash command data.
+                    if (slashCommand.configuration.deploy) {
+                        client.application?.commands.create(slashCommand.data);
+                        files += `, ${slashCommand.data.name}`;
+                    };
+                });
+
+                // Set the deployed state.
+                client.deployed = true;
+                message.channel.send({ embeds: [deployed.setDescription(files)] });
             });
-
-            // Set the deployed state.
-            client.deployed = true;
-        });
+        };
     };
 };
 
