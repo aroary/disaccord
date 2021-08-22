1a4beaec9b23327c5d4f99f0cc3072295a42ec52
aroary <meanchemshkedi@gmail.com>
2021-08-22 16:12:50 -0400

    log bool

--- core/bot/server/events/interactionCreate.js
+++ core/bot/server/events/interactionCreate.js
@@ -18,8 +18,8 @@ function run(client, interaction) {
     if (interaction.isCommand()) {
         const command = client.slashCommands.get(interaction.commandName);
         if (command) {
-            command(client, interaction);
-            new Entry("slash", interaction.commandName).setColor("purple").log();
+            command.run(client, interaction);
+            if (command.configuration.log) new Entry("slash", interaction.commandName).setColor("purple").log();
         };
     };
 
@@ -27,8 +27,8 @@ function run(client, interaction) {
     if (interaction.isButton()) {
         const button = client.buttons.get(interaction.customId);
         if (button) {
-            button(client, interaction);
-            new Entry("button", interaction.customId).setColor("purple").log();
+            button.run(client, interaction);
+            if(button.configuration.log) new Entry("button", interaction.customId).setColor("purple").log();
         };
     };
 
