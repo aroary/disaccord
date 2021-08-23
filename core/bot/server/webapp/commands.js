8086e47a2fc7fb626cdc681a0fafcd5b9d57cd03
aroary <meanchemshkedi@gmail.com>
2021-08-23 10:34:18 -0400

    example app (cmds)

---
+++ core/bot/server/webapp/commands.js
@@ -0,0 +1,24 @@
+const discord = require("discord.js");
+const path = require("path");
+const express = require("express");
+
+
+/**
+ * @description Commands.
+ * @param {discord.Client} client - The client.
+ * @param {express.Request} req - The request.
+ * @param {express.Response} res - The response.
+ */
+function send(client, req, res) {
+    const data = require("../../../data/publicData").data(client);
+    data.commands = client.commands;
+    res.status(200).set('Content-Type', 'text/html').render(path.join(__dirname, "../../client/webPages/commands/commands.ejs"), data);
+};
+
+module.exports = {
+    configuration: {
+        path: ["/commands"],
+        static: "core/bot/client/webPages/commands"
+    },
+    send
+};
