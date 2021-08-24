173fc4f9de48f6b65ed30512d84f02477647a22d
aroary <meanchemshkedi@gmail.com>
2021-08-24 17:46:48 -0400

    ws, logging

--- core/bot/server/webapp/commands.js
+++ core/bot/server/webapp/commands.js
@@ -1,6 +1,7 @@
 const discord = require("discord.js");
 const path = require("path");
 const express = require("express");
+const Entry = require("../../../utilities/logger");
 
 
 /**
@@ -13,6 +14,7 @@ function send(client, req, res) {
     const data = require("../../../data/publicData").data(client);
     data.commands = client.commands;
     res.status(200).set('Content-Type', 'text/html').render(path.join(__dirname, "../../client/webPages/commands/commands.ejs"), data);
+    new Entry("webapp", req.url).setColor("brown").log();
 };
 
 module.exports = {
