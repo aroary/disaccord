3e4e083e5afb9e0fda18049a7040acea50ac8bc5
aroary <meanchemshkedi@gmail.com>
2021-08-26 14:06:47 -0400

    reaction initiation, handling (support)

---
+++ core/bot/server/events/messageReactionAdd.js
@@ -0,0 +1,27 @@
+const discord = require("discord.js");
+const Entry = require("../../../utilities/logger");
+
+/**
+ * @description Messages
+ * @param {discord.Client} client - The client.
+ * @param {discord.MessageReaction} reaction - The message.
+ * @param {discord.User} user - The user who reacted.
+ */
+function run(client, reaction, user) {
+    // Return if the author is a bot.
+    if (user.bot) return;
+
+    // Check for reaction triggers.
+    client.reactions.forEach(r => {
+        if (r.execute.conditions(client, reaction, user) && (r.config.available || client.secrets.developers.includes(reaction.message.author.id.toString()))) {
+            if ((reaction.message.guild && r.config.server) || (!reaction.message.guild && r.config.direct)) {
+                r.execute.run(client, message);
+                if (r.config.log) new Entry("reaction", r.config.name).setColor("purple").log();
+            } else {
+                // Bad location.
+            };
+        };
+    });
+};
+
+module.exports = { name: "messageReactionAdd", run };
